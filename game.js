07abec1054a7503f559661f164079835c3cbef29
Yousef <177961328+yyusef5@users.noreply.github.com>
2026-08-03 02:25:25 +0300

    Add new function to game.js file
    I added new function named getComputerChoice which generate random choice
    of the Computer and return it

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-console.log("from js file ");
+function getComputerChoice()
+{
+    const choice = (Math.random() * 2).toFixed(0) ;
+    
+
+    if(choice == 0)
+        return 'Rock';
+    else if(choice == 1)
+        return 'Paper';
+    return 'Scissors';
+}
+
