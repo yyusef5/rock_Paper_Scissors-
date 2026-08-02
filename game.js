4cd154efca405a25bcacb3accab0b1d719fd308b
Yousef <177961328+yyusef5@users.noreply.github.com>
2026-08-03 02:43:22 +0300

    Add new function named PlayRound this function determine wether human or computer win this round

--- game.js
+++ game.js
@@ -4,10 +4,10 @@ function getComputerChoice()
     
 
     if(choice == 0)
-        return 'Rock';
+        return 'rock';
     else if(choice == 1)
-        return 'Paper';
-    return 'Scissors';
+        return 'paper';
+    return 'scissors';
 }
 
 function getHumanChoice()
@@ -16,4 +16,18 @@ function getHumanChoice()
 
     return choice.toLowerCase();
 }
+
+function PlayRound(ComputerChoice , HumanChoice)
+{
+    if(HumanChoice === ComputerChoice)
+        return 'equal';
+    else if(HumanChoice === 'rock' && ComputerChoice === 'scissors')
+        return 'human';
+    else if(HumanChoice === 'paper' && ComputerChoice === 'rock')
+        return 'human';
+    else if(HumanChoice === 'scissors' && ComputerChoice === 'paper')
+        return 'human';
+    return 'computer';
+}
+
 
